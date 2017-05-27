b691269bf4f1b565b804b03dff70246888892558
Fabien LOISON <flo@flogisoft.com>
2017-05-27 22:16:00 +0200

    wip CLI refacto

--- rivalcfg/__main__.py
+++ rivalcfg/__main__.py
@@ -1,5 +1,29 @@
+import sys
+
 from . import cli
+from . import list_available_mice, get_mouse
+
+
+def get_first_available_mouse():
+    available_mice = list(list_available_mice())
+    if not available_mice:
+        return None
+    return get_mouse(
+            available_mice[0].vendor_id,
+            available_mice[0].product_id
+            )
+
+
+def main(argv):
+    mouse = get_first_available_mouse()
+    profile = mouse.profile if mouse else None
+    parser = cli.generate_cli(profile)
+    options, args = parser.parse_args(argv)
+    print options
+    print args
+    pass
+
 
 if __name__ == "__main__":
-    cli.main()
+    main(sys.argv)
 
