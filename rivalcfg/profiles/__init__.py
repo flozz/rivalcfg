828637a35f21dc5b45171bcad6ca0589769b54d0
Soham Sen <sohamsen1000@gmail.com>
2018-05-26 23:16:48 +0530

    Added support for Rival 300 HP Omen Edition

--- rivalcfg/profiles/__init__.py
+++ rivalcfg/profiles/__init__.py
@@ -5,6 +5,7 @@ from .rival300 import rival300
 from .rival310 import rival310
 from .rival300csgofadeedition import rival300csgofadeedition
 from .rival300csgohyperbeastedition import rival300csgohyperbeastedition
+from .rival300hpomen import rival300hpomen
 from .rival500 import rival500
 from .hotssenseiraw import hotssenseiraw
 
@@ -15,6 +16,7 @@ mice_profiles = [
     rival300,
     rival300csgofadeedition,
     rival300csgohyperbeastedition,
+    rival300hpomen,
     rival310,
     rival500,
     hotssenseiraw,
