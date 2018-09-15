18838060d8f545adc6dddab7252afce497f203ba
Chris Bode <chris@eonz.net>
2018-09-14 21:29:30 -0400

    added sensitivity commands from rival600 to rival500
    The commands worked as-is on my machine with my rival500.

--- rivalcfg/profiles/rival500.py
+++ rivalcfg/profiles/rival500.py
@@ -10,6 +10,32 @@ rival500 = {
 
     "commands": {
 
+        "set_sensitivity1": {
+            "description": "Set sensitivity preset 1",
+            "cli": ["-s", "--sensitivity1"],
+            "command": [0x03, 0x00, 0x01],
+            "suffix": [0x00, 0x42],
+            "value_type": "range",
+            "range_min": 100,
+            "range_max": 12000,
+            "range_increment": 100,
+            "value_transform": lambda x: int((x / 100) - 1),
+            "default": 800,
+        },
+
+        "set_sensitivity2": {
+            "description": "Set sensitivity preset 2",
+            "cli": ["-S", "--sensitivity2"],
+            "command": [0x03, 0x00, 0x02],
+            "suffix": [0x00, 0x42],
+            "value_type": "range",
+            "range_min": 100,
+            "range_max": 12000,
+            "range_increment": 100,
+            "value_transform": lambda x: int((x / 100) - 1),
+            "default": 1600,
+        },
+
         "set_logo_color": {
             "description": "Set the logo backlight color",
             "cli": ["-c", "--logo-color"],
