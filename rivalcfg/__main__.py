7adf2975951be7c180b85b99efca144fa1e811ce
Fabien LOISON <flo@flogisoft.com>
2020-05-23 18:09:44 +0200

    First working version of the CLI :D

--- rivalcfg/__main__.py
+++ rivalcfg/__main__.py
@@ -2,16 +2,34 @@ import sys
 import argparse
 
 from . import cli
+from . import devices
+from . import get_first_mouse
 
 
 def main(args):
+    mouse = get_first_mouse()
+
     cli_parser = argparse.ArgumentParser(prog="rivalcfg")
     cli.add_main_cli(cli_parser)
-    from . import devices  # FIXME
-    mouse_profile = devices.get_profile(product_id=0x1702)  # FIXME
-    cli.add_mouse_cli(cli_parser, mouse_profile)  # FIXME
-    params = cli_parser.parse_args(args)
-    print(params)
+
+    if mouse:
+        mouse_profile = devices.get_profile(
+                vendor_id=mouse.vendor_id,
+                product_id=mouse.product_id)
+        cli.add_mouse_cli(cli_parser, mouse_profile)
+
+    settings = cli_parser.parse_args(args)
+
+    # Apply settings
+    if mouse:
+        for setting_name, value in [
+                (k.lower(), v) for k, v in vars(settings).items()]:
+            if value is None:
+                continue
+            method_name = "set_%s" % setting_name
+            if not hasattr(mouse, method_name):
+                continue
+            getattr(mouse, method_name)(value)
 
 
 if __name__ == "__main__":
