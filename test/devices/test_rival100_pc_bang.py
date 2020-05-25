3170f760eebc09cbb12342b2f1d7b0296bcf8552
Fabien LOISON <flo@flogisoft.com>
2020-05-25 13:57:50 +0200

    Adds tests for Rival 100 PC Bang to avoid regressions

---
+++ test/devices/test_rival100_pc_bang.py
@@ -0,0 +1,72 @@
+import pytest
+
+from rivalcfg import usbhid
+from rivalcfg import mouse
+from rivalcfg.devices import rival100_pc_bang
+
+
+class TestDevice(object):
+
+    @pytest.fixture
+    def mouse(self):
+        return mouse.Mouse(usbhid.FakeDevice(), rival100_pc_bang.profile)
+
+    @pytest.mark.parametrize("value,expected_hid_report", [
+        (250, b"\x02\x00\x03\x01\x08"),
+        (500, b"\x02\x00\x03\x01\x07"),
+        (1000, b"\x02\x00\x03\x01\x06"),
+        (1250, b"\x02\x00\x03\x01\x05"),
+        (1500, b"\x02\x00\x03\x01\x04"),
+        (1750, b"\x02\x00\x03\x01\x03"),
+        (2000, b"\x02\x00\x03\x01\x02"),
+        (4000, b"\x02\x00\x03\x01\x01"),
+        ])
+    def test_set_sensitivity1(self, mouse, value, expected_hid_report):
+        mouse.set_sensitivity1(value)
+        mouse._hid_device.bytes.seek(0)
+        hid_report = mouse._hid_device.bytes.read()
+        assert hid_report == expected_hid_report
+
+    @pytest.mark.parametrize("value,expected_hid_report", [
+        (250, b"\x02\x00\x03\x02\x08"),
+        (500, b"\x02\x00\x03\x02\x07"),
+        (1000, b"\x02\x00\x03\x02\x06"),
+        (1250, b"\x02\x00\x03\x02\x05"),
+        (1500, b"\x02\x00\x03\x02\x04"),
+        (1750, b"\x02\x00\x03\x02\x03"),
+        (2000, b"\x02\x00\x03\x02\x02"),
+        (4000, b"\x02\x00\x03\x02\x01"),
+        ])
+    def test_set_sensitivity2(self, mouse, value, expected_hid_report):
+        mouse.set_sensitivity2(value)
+        mouse._hid_device.bytes.seek(0)
+        hid_report = mouse._hid_device.bytes.read()
+        assert hid_report == expected_hid_report
+
+    @pytest.mark.parametrize("value,expected_hid_report", [
+        (125, b"\x02\x00\x04\x00\x04"),
+        (250, b"\x02\x00\x04\x00\x03"),
+        (500, b"\x02\x00\x04\x00\x02"),
+        (1000, b"\x02\x00\x04\x00\x01"),
+        ])
+    def test_set_polling_rate(self, mouse, value, expected_hid_report):
+        mouse.set_polling_rate(value)
+        mouse._hid_device.bytes.seek(0)
+        hid_report = mouse._hid_device.bytes.read()
+        assert hid_report == expected_hid_report
+
+    @pytest.mark.parametrize("value,expected_hid_report", [
+        ("dpi", b"\x02\x00\x0B\x00"),
+        ("os", b"\x02\x00\x0B\x01"),
+        ])
+    def test_set_btn6_mode(self, mouse, value, expected_hid_report):
+        mouse.set_btn6_mode(value)
+        mouse._hid_device.bytes.seek(0)
+        hid_report = mouse._hid_device.bytes.read()
+        assert hid_report == expected_hid_report
+
+    def test_save(self, mouse):
+        mouse.save()
+        mouse._hid_device.bytes.seek(0)
+        hid_report = mouse._hid_device.bytes.read()
+        assert hid_report == b"\x02\x00\x09\x00"
