74db83fc6135f3dcd3c05078a55a977313564d9e
nixtux <davehomer@googlemail.com>
2020-07-18 20:12:34 +0100

    remove duplicate code

--- rivalcfg/handlers/rival700_rgbgradient.py
+++ rivalcfg/handlers/rival700_rgbgradient.py
@@ -109,79 +109,20 @@ import argparse
 
 from ..helpers import uint_to_little_endian_bytearray, merge_bytes
 from ..helpers import bytes_to_high_low_nibbles, nibbles_to_byte
-from ..helpers import parse_param_string, REGEXP_PARAM_STRING
-from ..color_helpers import is_color, parse_color_string
-from ..color_helpers import parse_color_gradient_string
+from ..color_helpers import is_color
+from .rgbgradient import _handle_color_tuple, _handle_color_string
+from .rgbgradient import _handle_rgbgradient_dict, _handle_rgbgradient_string
+from .rgbgradient import is_rgbgradient, CheckGradientAction
+
 
 
 # Compatibility with Python 2.7.
 # On Python 2 the type is 'unicode'
 # On Python 3 the type is 'str'
+
 _unicode_type = type(u"")
 
 _default_duration = 1000
-
-
-def _handle_color_tuple(color):
-    if len(color) != 3:
-        raise ValueError("Not a valid color %s" % str(color))
-    for channel in color:
-        if type(channel) != int or channel < 0 or channel > 255:
-            raise ValueError("Not a valid color %s" % str(color))
-    return [{
-        "pos": 0,
-        "color": color,
-    }]
-
-
-def _handle_color_string(color):
-    return [{
-        "pos": 0,
-        "color": parse_color_string(color),
-    }]
-
-
-def _handle_rival700_rgbgradient_dict(colors):
-    duration = _default_duration
-    gradient = []
-
-    if "duration" in colors:
-        duration = colors["duration"]
-
-    if "colors" in colors:
-        for stop in colors["colors"]:
-            color = stop["color"]
-            if type(color) in [str, _unicode_type]:
-                color = parse_color_string(color)
-            if type(color) not in [tuple, list] or len(color) != 3:
-                raise ValueError("Not a valid color %s" % str(color))
-            for channel in color:
-                if type(channel) != int or channel < 0 or channel > 255:
-                    raise ValueError("Not a valid color %s" % str(color))
-            gradient.append({
-                "pos": stop["pos"] if "pos" in stop else 0,
-                "color": color
-            })
-
-    # Smooth gradient (if possible) by adding a final color
-    if len(gradient) < 14 and gradient[-1]["pos"] != 100:
-        gradient.append({
-            "pos": 100,
-            "color": gradient[0]["color"],
-            })
-
-    return duration, gradient
-
-
-def _handle_rival700_rgbgradient_string(colors):
-    gradient_dict = parse_param_string(colors, value_parsers={
-            "rgbgradient": {
-                "duration": int,
-                "colors": parse_color_gradient_string,
-            }
-        })
-
-    return _handle_rival700_rgbgradient_dict(gradient_dict["rgbgradient"])
 
 
 def process_value(setting_info, colors):
@@ -209,11 +150,11 @@ def process_value(setting_info, colors):
 
     # Color gradient as dict
     elif type(colors) is dict:
-        duration, gradient = _handle_rival700_rgbgradient_dict(colors)
+        duration, gradient = _handle_rgbgradient_dict(colors)
 
     # Color gradient as string
     elif is_rgbgradient(colors)[0]:
-        duration, gradient = _handle_rival700_rgbgradient_string(colors)
+        duration, gradient = _handle_rgbgradient_string(colors)
     else:
         raise ValueError("Not a valid color or rgbgradient %s" % str(colors))
 
@@ -295,78 +236,6 @@ def process_value(setting_info, colors):
     suffix = merge_bytes(split_color, end_suffix, duration)
 
     return merge_bytes(header, suffix)
-
-
-def is_rgbgradient(string):
-    """Check if the regbradient expression is valid.
-
-    :param str string: The string to validate.
-    :rtype: (bool, str)
-
-    >>> is_rgbgradient("foo(colors=0:red)")
-    (False, "... It must looks like 'rgbgradient(<PARAMS>)'...")
-    >>> is_rgbgradient("rgbgradient(duration=1000)")
-    (False, "... You must provide at least one color: ...")
-    >>> is_rgbgradient("rgbgradient(colors=red)")
-    (False, "invalid color gradient...")
-    >>> is_rgbgradient("rgbgradient(colors=0:red; foo=bar)")
-    (False, "unknown parameter 'foo'...")
-    """
-    try:
-        rgbgradient_dict = parse_param_string(string, value_parsers={
-            "rgbgradient": {
-                "duration": int,
-                "colors": parse_color_gradient_string,
-            }
-        })
-    except ValueError as e:
-        return False, str(e)
-
-    if "rgbgradient" not in rgbgradient_dict:
-        reason = ""
-        reason += "invalid rgbgradient expression. "
-        reason += "It must looks like 'rgbgradient(<PARAMS>)'."
-        return False, reason
-
-    if "colors" not in rgbgradient_dict["rgbgradient"] \
-            or len(rgbgradient_dict["rgbgradient"]) == 0:
-        reason = ""
-        reason += "invalid rgbgradient expression. "
-        reason += "You must provide at least one color: "
-        reason += "'rgbgradient(colors=<POS>: <COLOR>)'."
-        return False, reason
-
-    _allowed_params = ["colors", "duration"]
-    for key in rgbgradient_dict["rgbgradient"].keys():
-        if key not in _allowed_params:
-            reason = ""
-            reason += "unknown parameter '%s'. " % key
-            reason += "Allowed parameters are %s." % ", ".join(
-                    ["'%s'" % p for p in _allowed_params])
-            return False, reason
-
-    return True, ""
-
-
-class CheckGradientAction(argparse.Action):
-    """Validate colors gradient from CLI"""
-
-    def __call__(self, parser, namespace, value, option_string=None):
-        if is_color(value):
-            setattr(namespace, self.dest.upper(), value)
-            return
-
-        if REGEXP_PARAM_STRING.match(value):
-            is_valid, reason = is_rgbgradient(value)
-
-            if is_valid:
-                setattr(namespace, self.dest.upper(), value)
-                return
-
-            raise argparse.ArgumentError(self, "%s" % reason)
-
-        else:
-            raise argparse.ArgumentError(self, "not a valid color or rgbgradient: '%s'" % value)  # noqa
 
 
 def add_cli_option(cli_parser, setting_name, setting_info):
