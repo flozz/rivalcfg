672e3a6bfee6f0c8bb3b3eda884fcaf2386b5232
nixtux <davehomer@googlemail.com>
2020-07-29 09:35:17 +0100

    Initial support for the rival 700s

---
+++ rivalcfg/devices/rival700.py
@@ -0,0 +1,71 @@
+from .. import usbhid
+
+
+profile = {
+
+    "name": "SteelSeries Rival 700",
+
+    "models": [{
+        "name": "SteelSeries Rival 700",
+        "vendor_id": 0x1038,
+        "product_id": 0x1700,
+        "endpoint": 0,
+    }, {
+        "name": "SteelSeries Rival 710",
+        "vendor_id": 0x1038,
+        "product_id": 0x1730,
+        "endpoint": 0,
+    }],
+
+    "settings": {
+
+        "sensitivity1": {
+            "label": "Sensibility preset 1",
+            "description": "Set sensitivity preset 1 (DPI)",
+            "cli": ["-s", "--sensitivity1"],
+            "report_type": usbhid.HID_REPORT_TYPE_OUTPUT,
+            "command": [0x03, 0x00, 0x01],
+            "command_suffix": [0x00, 0x42],
+            "value_type": "range",
+            "input_range": [100, 12000, 100],
+            "output_range": [0x00, 0x77, 1],
+            "default": 800,
+        },
+
+        "sensitivity2": {
+            "label": "Sensibility preset 2",
+            "description": "Set sensitivity preset 2 (DPI)",
+            "cli": ["-S", "--sensitivity2"],
+            "report_type": usbhid.HID_REPORT_TYPE_OUTPUT,
+            "command": [0x03, 0x00, 0x02],
+            "command_suffix": [0x00, 0x42],
+            "value_type": "range",
+            "input_range": [100, 12000, 100],
+            "output_range": [0x00, 0x77, 1],
+            "default": 1600,
+        },
+
+        "polling_rate": {
+            "label": "Polling rate",
+            "description": "Set polling rate (Hz)",
+            "cli": ["-p", "--polling-rate"],
+            "report_type": usbhid.HID_REPORT_TYPE_OUTPUT,
+            "command": [0x04, 0x00],
+            "value_type": "choice",
+            "choices": {
+                125: 0x04,
+                250: 0x03,
+                500: 0x02,
+                1000: 0x01,
+            },
+            "default": 1000,
+        },
+
+    },
+
+    "save_command": {
+        "report_type": usbhid.HID_REPORT_TYPE_OUTPUT,
+        "command": [0x09, 0x00],
+    },
+
+}
