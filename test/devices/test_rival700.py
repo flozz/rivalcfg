35fd711d03b6a8150d4aa90414ef412fad58026b
Fabien LOISON <flo@flogisoft.com>
2020-08-03 18:11:02 +0200

    rival700: adds tests

--- test/devices/test_rival700.py
+++ test/devices/test_rival700.py
@@ -47,6 +47,90 @@ class TestDevice(object):
         hid_report = mouse._hid_device.bytes.read()
         assert hid_report == expected_hid_report
 
+    def test_set_logo_color(self, mouse):
+        mouse.set_logo_color("rgbgradient(duration=1000; colors=0%: #ff0000, 33%: #00ff00, 66%: #0000ff)")  # noqa
+
+        mouse._hid_device.bytes.seek(0)
+        hid_report = mouse._hid_device.bytes.read()
+
+        expected_hid_report = b""
+        expected_hid_report += b"\x03\x00\x05\x00"
+        expected_hid_report += b"\x00\x1D\x01\x02\x31\x51\xFF\xC8\x00\x00"
+        expected_hid_report += b"\x00\xF4\x0C\x00\x00\x4A\x01\x01\x00\x00"
+        expected_hid_report += b"\xF4\x0C\x00\x4A\x01\x02\x00\x0C\x00\xF4"
+        expected_hid_report += b"\x00\x54\x01\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\xF0"
+        expected_hid_report += b"\x0F\x00\x00\x00\x00\xFF\x00\xDC\x05\x8A"
+        expected_hid_report += b"\x02\x00\x00\x00\x00\x01\x00\x03\x00\xE8"
+        expected_hid_report += b"\x03"
+
+        assert hid_report == expected_hid_report
+
+    def test_set_wheel_color(self, mouse):
+        mouse.set_wheel_color("rgbgradient(duration=5000; colors=0%: #112233, 25%: #445566, 50%: #778899, 75%: #AABBCC)")  # noqa
+
+        mouse._hid_device.bytes.seek(0)
+        hid_report = mouse._hid_device.bytes.read()
+
+        expected_hid_report = b""
+        expected_hid_report += b"\x03\x00\x05\x00"
+        expected_hid_report += b"\x01\x1D\x01\x02\x31\x51\xFF\xC8\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\xE2\x04\x01\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\xE2\x04\x02\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\xE2\x04\x03\x00\xFF\xFF\xFF\x00\xE2"
+        expected_hid_report += b"\x04\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x10"
+        expected_hid_report += b"\x01\x20\x02\x30\x03\xFF\x00\xDC\x05\x8A"
+        expected_hid_report += b"\x02\x00\x00\x00\x00\x01\x00\x04\x00\x88"
+        expected_hid_report += b"\x13"
+
+        assert hid_report == expected_hid_report
+
+    def test_set_wheel_color_with_color_string(self, mouse):
+        mouse.set_wheel_color("#FF1800")
+
+        mouse._hid_device.bytes.seek(0)
+        hid_report = mouse._hid_device.bytes.read()
+
+        expected_hid_report = b""
+        expected_hid_report += b"\x03\x00\x05\x00"
+        expected_hid_report += b"\x01\x1D\x01\x02\x31\x51\xFF\xC8\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        expected_hid_report += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\xF0"
+        expected_hid_report += b"\x0F\x80\x01\x00\x00\xFF\x00\xDC\x05\x8A"
+        expected_hid_report += b"\x02\x00\x00\x00\x00\x01\x00\x00\x00\xE8"
+        expected_hid_report += b"\x03"
+
+        assert hid_report == expected_hid_report
+
     def test_save(self, mouse):
         mouse.save()
         mouse._hid_device.bytes.seek(0)
