25bfcfd0936c04e5ac3338c6e60fcf4ae1c08101
Fabien LOISON <flo@flogisoft.com>
2020-11-07 14:22:27 +0100

    Sensei TEN: Color suport implemented

--- rivalcfg/devices/sensei_ten.py
+++ rivalcfg/devices/sensei_ten.py
@@ -1,6 +1,19 @@
 from .. import usbhid
 
 
+_RGBGRADIENT_HEADER = {
+    "header_length": 26,       # Length of the header excuding command / LED ID
+    "led_id_offsets": [0],     # Offset of the "led_id" fields
+    "duration_offset": 1,      # Offset of the "duration" field
+    "duration_length": 2,      # Length of the "duration" field (in Bytes)
+    "repeat_offset": 17,       # Offset of the "repeat" flag
+    "triggers_offset": 21,     # Offset of the "triggers" field (buttons mask)
+    "color_count_offset": 25,  # Offset of the "color_count" field
+}
+
+_DEFAULT_RGBGRADIENT = "rgbgradient(duration=1000; colors=0%: #ff0000, 33%: #00ff00, 66%: #0000ff)"  # noqa
+
+
 profile = {
 
     "name": "SteelSeries Sensei TEN",
@@ -48,6 +61,30 @@ profile = {
                 1000: 0x01,
             },
             "default": 1000,
+        },
+
+        "logo_color": {
+            "label": "Logo LED colors and effects",
+            "description": "Set the colors and the effects of the logo LED",
+            "cli": ["-c", "--logo-color"],
+            "report_type": usbhid.HID_REPORT_TYPE_FEATURE,
+            "command": [0x5B, 0x00],
+            "value_type": "rgbgradient",
+            "rgbgradient_header": _RGBGRADIENT_HEADER,
+            "led_id": 0x00,
+            "default": _DEFAULT_RGBGRADIENT,
+        },
+
+        "wheel_color": {
+            "label": "Wheel LED colors and effects",
+            "description": "Set the colors and the effects of the wheel LED",
+            "cli": ["-C", "--wheel-color"],
+            "report_type": usbhid.HID_REPORT_TYPE_FEATURE,
+            "command": [0x5B, 0x00],
+            "value_type": "rgbgradient",
+            "rgbgradient_header": _RGBGRADIENT_HEADER,
+            "led_id": 0x01,
+            "default": _DEFAULT_RGBGRADIENT,
         },
 
     },
