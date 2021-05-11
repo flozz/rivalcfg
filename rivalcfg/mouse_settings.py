f82bd3450dcc2403e3de4bf9129d7e0a7478124f
Fabien LOISON <flo@flogisoft.com>
2021-05-11 14:00:54 +0200

    MouseSettings: get default values

--- rivalcfg/mouse_settings.py
+++ rivalcfg/mouse_settings.py
@@ -17,13 +17,23 @@ def get_settings_path(vendor_id, product_id):
 
 
 class MouseSettings(object):
-    def __init__(self, vendor_id, product_id, mouse_profile):
-        raise NotImplementedError()
+    def __init__(
+        self,
+        vendor_id,
+        product_id,
+        mouse_profile,
+        current_profile_name="default",
+    ):
+        self._mouse_profile = mouse_profile
+        self._settings_path = get_settings_path(vendor_id, product_id)
+        self._current_profile_name = current_profile_name
+        self._settings = {}
+        self._load()
 
     def list_settings_profiles(self):
         raise NotImplementedError()
 
-    def get_settings_profile(self, name="default"):
+    def set_active_profile(self, profile_name):
         raise NotImplementedError()
 
     def create_settings_profile(self, name, from_profile_name=None):
@@ -34,15 +44,50 @@ class MouseSettings(object):
         # TODO default profile cannot be removed
         raise NotImplementedError()
 
+    def get_default_values(self):
+        return {
+            k: v["default"] for k, v in self._mouse_profile["settings"].items()
+        }
+
     def set(self, setting_name, value):
         raise NotImplementedError()
 
     def get(self, setting_name):
         raise NotImplementedError()
 
+    def save(self):
+        raise NotImplementedError()
+
+    def _load(self):
+        raise NotImplementedError()
+
 
 class FakeMouseSettings(MouseSettings):
-    def __init__(self, vendor_id, product_id, mouse_profile):
-        self._moues_profile = mouse_profile
-        self._mouse_id = "%04x_%04x" % (vendor_id, product_id)
-        self._settings_profiles = {}
+    def save(self):
+        pass
+
+    def _load(self):
+        self._settings = {
+            "default": self.get_default_values(),
+        }
+
+
+def get_mouse_settings(
+    vendor_id,
+    product_id,
+    mouse_profile,
+    current_profile_name="default",
+):
+    if "RIVALCFG_DRY" in os.environ:
+        return FakeMouseSettings(
+            vendor_id,
+            product_id,
+            mouse_profile,
+            current_profile_name
+        )
+    return MouseSettings(
+        vendor_id,
+        product_id,
+        mouse_profile,
+        current_profile_name
+    )
--- test/test_mouse_settings.py
+++ test/test_mouse_settings.py
@@ -1,4 +1,17 @@
+import pytest
+
 from rivalcfg import mouse_settings
+
+FAKE_PROFILE = {
+    "settings": {
+        "setting1": {
+            "default": "foo",
+        },
+        "setting2": {
+            "default": "bar",
+        },
+    },
+}
 
 
 class Test_get_xdg_config_home(object):
@@ -19,3 +32,16 @@ class Test_get_settings_path(object):
             mouse_settings.get_settings_path(0x1038, 0xbaad)
             == "/tmp/foo/bar/rivalcfg/1038_baad.device.json"
         )
+
+
+class Test_MouseSettings(object):
+
+    @pytest.fixture
+    def mouse_settings(self):
+        return mouse_settings.get_mouse_settings(0x1038, 0xbaad, FAKE_PROFILE)
+
+    def test_get_default_values(self, mouse_settings):
+        assert mouse_settings.get_default_values() == {
+            "setting1": "foo",
+            "setting2": "bar",
+        }
