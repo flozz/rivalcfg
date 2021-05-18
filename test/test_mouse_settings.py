375ea094f6ec81fe686785bd2858c3bfe11277a8
Fabien LOISON <flo@flogisoft.com>
2021-05-18 12:32:36 +0200

    Black fix

--- test/test_mouse_settings.py
+++ test/test_mouse_settings.py
@@ -2,6 +2,7 @@ import pytest
 
 from rivalcfg import mouse_settings
 
+
 FAKE_PROFILE = {
     "settings": {
         "setting1": {
@@ -29,16 +30,15 @@ class Test_get_settings_path(object):
     def test_settings_file_path(self, monkeypatch):
         monkeypatch.setenv("XDG_CONFIG_HOME", "/tmp/foo/bar")
         assert (
-            mouse_settings.get_settings_path(0x1038, 0xbaad)
+            mouse_settings.get_settings_path(0x1038, 0xBAAD)
             == "/tmp/foo/bar/rivalcfg/1038_baad.device.json"
         )
 
 
 class Test_MouseSettings(object):
-
     @pytest.fixture
     def mouse_settings(self):
-        return mouse_settings.get_mouse_settings(0x1038, 0xbaad, FAKE_PROFILE)
+        return mouse_settings.get_mouse_settings(0x1038, 0xBAAD, FAKE_PROFILE)
 
     def test_get_default_values(self, mouse_settings):
         assert mouse_settings.get_default_values() == {
