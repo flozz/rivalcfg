7e3fdcdfc9dcfb65d31cc292ca169d592b56b49c
Fabien LOISON <flo@flogisoft.com>
2021-08-18 13:53:58 +0200

    fix lint #2...

--- rivalcfg/devices/__init__.py
+++ rivalcfg/devices/__init__.py
@@ -145,7 +145,7 @@ Module API
 import os
 import types
 
-from . import aerox3
+from . import aerox3  # noqa: F401
 from . import kanav2  # noqa: F401
 from . import kinzuv2  # noqa: F401
 from . import rival3  # noqa: F401
