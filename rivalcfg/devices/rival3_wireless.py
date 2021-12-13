7618e6654a066c55f3f83ea883e82e59d7641b0a
Fabien LOISON <flo@flogisoft.com>
2021-12-13 18:15:33 +0100

    Rival 3 Wireless: initial work

---
+++ rivalcfg/devices/rival3_wireless.py
@@ -0,0 +1,50 @@
+from .. import usbhid
+
+
+profile = {
+    "name": "SteelSeries Rival 3 Wireless",
+    "models": [
+        {
+            "name": "SteelSeries Aerox 3 Wireless (wired mode)",
+            "vendor_id": 0x1038,
+            "product_id": 0x1830,
+            "endpoint": 0,
+        },
+    ],
+    "settings": {
+        "sensitivity": {
+            "label": "Sensibility presets",
+            "description": "Set sensitivity preset (DPI)",
+            "cli": ["-s", "--sensitivity"],
+            "report_type": usbhid.HID_REPORT_TYPE_OUTPUT,
+            "command": [0x20],
+            "value_type": "multidpi_range",
+            "input_range": [100, 18000, 100],
+            "output_range": [0x00, 0xD6, 1.2],
+            "dpi_length_byte": 2,
+            "first_preset": 1,
+            "count_mode": "number",
+            "max_preset_count": 5,
+            "default": "400, 800, 1200, 2400, 3200",
+        },
+        "polling_rate": {
+            "label": "Polling rate",
+            "description": "Set polling rate (Hz)",
+            "cli": ["-p", "--polling-rate"],
+            "report_type": usbhid.HID_REPORT_TYPE_OUTPUT,
+            "command": [0x17],
+            "value_type": "choice",
+            "choices": {
+                125: 0x03,
+                250: 0x02,
+                500: 0x01,
+                1000: 0x00,
+            },
+            "default": 1000,
+        },
+    },
+    # "save_command": {
+        # "report_type": usbhid.HID_REPORT_TYPE_OUTPUT,
+        # "command": [0x11, 0x00],
+    # },
+}
---
+++ test/devices/test_rival3_wireless.py
@@ -0,0 +1,62 @@
+import pytest
+
+from rivalcfg import usbhid
+from rivalcfg import mouse
+from rivalcfg.devices import rival3_wireless
+from rivalcfg import mouse_settings
+
+
+class TestDevice(object):
+    @pytest.fixture
+    def mouse(self):
+        settings = mouse_settings.FakeMouseSettings(
+            0x1038,
+            0xBAAD,
+            rival3_wireless.profile,
+        )
+        return mouse.Mouse(
+            usbhid.FakeDevice(),
+            rival3_wireless.profile,
+            settings,
+        )
+
+    @pytest.mark.parametrize(
+        "value,expected_hid_report",
+        [
+            (100, b"\x02\x00\x20\x01\x01\x00\x00"),
+            (200, b"\x02\x00\x20\x01\x01\x01\x00"),
+            (300, b"\x02\x00\x20\x01\x01\x02\x00"),
+            (18000, b"\x02\x00\x20\x01\x01\xD6\x00"),
+            ("200,400", b"\x02\x00\x20\x02\x01\x01\x00\x03\x00"),
+            (
+                "200,400,800,1600",
+                b"\x02\x00\x20\x04\x01\x01\x00\x03\x00\x08\x00\x11\x00",
+            ),
+        ],
+    )
+    def test_set_sensitivity(self, mouse, value, expected_hid_report):
+        mouse.set_sensitivity(value)
+        mouse._hid_device.bytes.seek(0)
+        hid_report = mouse._hid_device.bytes.read()
+        assert hid_report == expected_hid_report
+
+    @pytest.mark.parametrize(
+        "value,expected_hid_report",
+        [
+            (125, b"\x02\x00\x17\x03"),
+            (250, b"\x02\x00\x17\x02"),
+            (500, b"\x02\x00\x17\x01"),
+            (1000, b"\x02\x00\x17\x00"),
+        ],
+    )
+    def test_set_polling_rate(self, mouse, value, expected_hid_report):
+        mouse.set_polling_rate(value)
+        mouse._hid_device.bytes.seek(0)
+        hid_report = mouse._hid_device.bytes.read()
+        assert hid_report == expected_hid_report
+
+    # def test_save(self, mouse):
+        # mouse.save()
+        # mouse._hid_device.bytes.seek(0)
+        # hid_report = mouse._hid_device.bytes.read()
+        # assert hid_report == b"\x02\x00\x11\x00"
