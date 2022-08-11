25c1ecf32a52964c17f7a4ddf2011552e63a2e7e
Fabien LOISON <flo@flogisoft.com>
2022-08-11 13:27:24 +0200

    Add initial support of the Prime Wireless mode in wired mode

---
+++ rivalcfg/devices/prime_wireless_wired.py
@@ -0,0 +1,50 @@
+from .. import usbhid
+
+
+profile = {
+    "name": "SteelSeries Prime Wireless",
+    "models": [
+        {
+            "name": "SteelSeries Prime Wireless (wired mode)",
+            "vendor_id": 0x1038,
+            "product_id": 0x1840,
+            "endpoint": 3,
+        },
+    ],
+    "settings": {
+        "sensitivity": {
+            "label": "Sensibility presets",
+            "description": "Set sensitivity preset (DPI)",
+            "cli": ["-s", "--sensitivity"],
+            "report_type": usbhid.HID_REPORT_TYPE_OUTPUT,
+            "command": [0x2D],
+            "value_type": "multidpi_range",
+            "input_range": [100, 18000, 100],
+            "output_range": [0x00, 0xD6, 1.2],
+            "dpi_length_byte": 1,
+            "first_preset": 0,
+            "count_mode": "number",
+            "max_preset_count": 5,
+            "default": "400, 800, 1200, 2400, 3200",
+        },
+        "polling_rate": {
+            "label": "Polling rate",
+            "description": "Set polling rate (Hz)",
+            "cli": ["-p", "--polling-rate"],
+            "report_type": usbhid.HID_REPORT_TYPE_OUTPUT,
+            "command": [0x2B],
+            "value_type": "choice",
+            "choices": {
+                125: 0x03,
+                250: 0x02,
+                500: 0x01,
+                1000: 0x00,
+            },
+            "default": 1000,
+        },
+    },
+    "save_command": {
+        "report_type": usbhid.HID_REPORT_TYPE_OUTPUT,
+        "command": [0x11, 0x00],
+    },
+}
